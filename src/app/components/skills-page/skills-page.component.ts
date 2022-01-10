1c43d449522d49028cfb09ff8500b1786379d4df
Gabriel Tinoco <gabrielslai@gmail.com>
2022-01-10 17:08:47 -0500

    Fixed name.

--- src/app/components/skills-page/skills-page.component.ts
+++ src/app/components/skills-page/skills-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-services-page',
+  selector: 'app-skills-page',
   templateUrl: './skills-page.component.html',
   styleUrls: ['./skills-page.component.css']
 })
